abdd42a2b5f4714ac6b418b2092cc8e553b4109c
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2025-02-04 03:24:19 +0100

    fixed jwt token refresh bug

--- frontend/src/hooks/utilityFns.jsx
+++ frontend/src/hooks/utilityFns.jsx
@@ -31,7 +31,7 @@ api.interceptors.response.use(
     async (error) => {
         const originalRequest = error.config
 
-        if (error.response?.status === 403 && !originalRequest._retry) {
+        if ((error.response?.status === 401 || error.response?.status === 403) && !originalRequest._retry) {
             originalRequest._retry = true
             const newAccessToken = await refreshToken()
 
